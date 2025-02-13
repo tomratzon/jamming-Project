d5d08446907d561c7711071f2b6538d5d2c2f5d9
tomrat <tom.ratzon@gmail.com>
2025-02-13 12:13:52 +0200

    add loading screen for saving playlist and tracks that are int the playlist wont be in the search results

--- src/componnents/App/app.js
+++ src/componnents/App/app.js
@@ -1,4 +1,4 @@
-import React, {useState }  from 'react';
+import React, {useState,useEffect }  from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../PlayList/Playlist';
@@ -9,54 +9,71 @@ import './App.css'; // Assuming you have some CSS for basic styling
 function App() {
     //searchResults state decluration
   const [searchResults, setSearchResults] = useState([]);
-
-  const search = (term) => {
-    Spotify.search(term).then(searchResults => {
-      setSearchResults(searchResults);
-    });
-  };
+  const [filteredResults, setFilteredResults] = useState([]);
   //playlistName state decluration
   const [playlistName, setPlaylistName] = useState("My Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false); //used for implemanting loading
+
+  
+  const filterSearchResults = () => {
+    const filtered = searchResults.filter(searchTrack =>
+        !playlistTracks.find(playlistTrack => playlistTrack.id === searchTrack.id));
+    setFilteredResults(filtered);
+  };
+  
+  useEffect(() => {
+    filterSearchResults();
+  } , [playlistTracks, searchResults]);
+
+
+  const search = (track) => {
+    Spotify.search(track).then(results => {
+        setSearchResults(results);
+    });
+  };
+
   const addTrack= (track)=>{ //add track into playlistTracks if isn't there already
     const trackAlreadyExist = playlistTracks.find(pTrack => pTrack.id === track.id);
     if(!trackAlreadyExist){
       setPlaylistTracks([...playlistTracks,track]);
     }
   };
+
   const removeTrack = (track)=>{
     const newPlaylist=playlistTracks.filter(ptrack=>ptrack.id!==track.id);
     setPlaylistTracks(newPlaylist);
   };
+
   const updatePlaylistName= (name)=>{
     setPlaylistName(name);
   };
+
   const savePlaylist = () => {
     const trackUris = playlistTracks.map(track => track.uri);
+    if(playlistTracks.length===0){return;}//if playlist empty wont save it 
+    setIsLoading(true);//start loading feature
     Spotify.savePlaylist(playlistName, trackUris)
       .then(() => {
         setPlaylistName('New Playlist');
         setPlaylistTracks([]);
+        setIsLoading(false);//end loading features
       })
       .catch(error => {
         console.error('Error saving the playlist:', error);
       });
   };
   
-
-//heading 
-//searchBar
-//app-playlist
-//  search results
-//  playlist 
   return (
     <div className="App">
       <div className="Search-container">
         <h1>Ja<span className="highlight">mmm</span>ing</h1> 
         <SearchBar onSearch={search}/>
-        <SearchResults searchResults={searchResults} onAdd={addTrack} />
+        
       </div>
       <div className="App-playlist">
+        <SearchResults searchResults={filteredResults} onAdd={addTrack} />
         <Playlist 
           playlistName={playlistName} 
           playlistTracks={playlistTracks} 
@@ -64,6 +81,7 @@ function App() {
           onRemove={removeTrack}
           onSave={savePlaylist}
         />
+         {isLoading && <div className="LoadingOverlay">Saving {playlistName}...</div>}
     </div>
   </div>
   );
